46637f1beb4d2e81f31cde7f27743405d1271128
Ayan Enrqz <145316034+Ayannrwz@users.noreply.github.com>
2023-10-31 17:03:24 +0800

    Update SlotMachine.js

--- SlotMachine.js
+++ SlotMachine.js
@@ -3,6 +3,7 @@ class SlotMachine extends React.Component {
         const {s1, s2, s3} = this.props;
         let result = (s1 == s2) && (s2 == s3) && (s1 == s3);
         return (
+             <div className="slot-machine">
             <div className="data">
                 {s1}
                 {s2}
@@ -10,6 +11,7 @@ class SlotMachine extends React.Component {
                 {
                 result ? <p className="winner">Winner</p> : <p className="loser">Loser</p>
             } </div>
+                     </div>
         );
     }
 }
